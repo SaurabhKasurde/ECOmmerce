6539dfd352d620be931b88bce5b2842b5b081c01
shivamsubu <shrivastavashivam098@gmail.com>
2025-01-23 16:51:56 +0530

    tfytt

--- ECOmmerce/Controllers/TestController.cs
+++ ECOmmerce/Controllers/TestController.cs
@@ -12,5 +12,10 @@ namespace ECOmmerce.Controllers
         {
             return View();
         }
+
+        public IActionResult Sghj()
+        {
+            return View();
+        }
     }
 }
